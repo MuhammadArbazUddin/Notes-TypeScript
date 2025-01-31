80704af33883e2d8b24b488340e0649096105fc9
Arbaz <mauak44@gmail.com>
2025-01-31 18:22:35 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import NewNote from "./NewNote";
+
+function App() {
+  return (
+    <>
+      <Container className="my-4">
+        <Routes>
+          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/" element={<h1>Helloe</h1>} />
+          <Route path="/new" element={<NewNote />} />
+          <Route path="/:id">
+            <Route index element={<h1>Show</h1>} />
+            <Route path="edit" element={<h1>Edit</h1>} />
+          </Route>
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/NewNote.tsx
@@ -0,0 +1,11 @@
+import NewForm from "./NewForm";
+
+function NewNote() {
+  return (
+    <div>
+      <NewForm />
+    </div>
+  );
+}
+
+export default NewNote;
